3e9cb60009d4be67bc19fb2eae8df70e2d80fafa
Esben Baden Smith <esbenbsmith@gmail.com>
2015-08-11 11:00:47 -0700

    added posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,75 @@
 class PostsController < ApplicationController
+  before_filter :authorize, except: [:index, :show]
+
+  # show ALL posts in db
+  def index
+    @posts = Post.all
+    render :index
+  end
+
+  # form to create new post
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  # creates new post in db
+  # that BELONGS TO current_user
+  def create
+    # post = post.create(post_params)
+    # current_user.posts << post
+    post = current_user.posts.create(post_params)
+    redirect_to post_path(post)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render :show
+  end
+
+  # form to edit one post
+  def edit
+    @post = Post.find(params[:id])
+    if current_user.posts.include? @post
+      render :edit
+    else
+      # if user tries to edit post that doesn't belong to them
+      # log them out
+      session[:user_id] = nil
+      redirect_to login_path
+    end
+  end
+
+  # updates post in db
+  def update
+    post = Post.find(params[:id])
+    if current_user.posts.include? post
+      post.update_attributes(post_params)
+      redirect_to post_path(post)
+    else
+      # if user tries to update post that doesn't belong to them
+      # log them out
+      session[:user_id] = nil
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if current_user.posts.include? post
+      post.destroy
+      redirect_to profile_path
+    else
+      # if user tries to destroy post that doesn't belong to them
+      # log them out
+      session[:user_id] = nil
+      redirect_to login_path
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
 end
