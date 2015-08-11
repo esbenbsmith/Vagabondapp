91b8565a3ae8b3dbafcc1a18e80a682a3ac1fad9
Amanda <amandaannegeorge@gmail.com>
2015-08-10 17:40:21 -0700

    added signup view and user new and create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to profile_path
+    else
+      redirect_to signup_path
+    end
   end
 
   def show
@@ -13,4 +20,10 @@ class UsersController < ApplicationController
 
   def update
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+
 end
