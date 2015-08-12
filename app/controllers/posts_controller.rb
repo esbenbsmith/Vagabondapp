8f18a46eebcec641244ae493f8dbae8f9a54f69f
Esben Baden Smith <esbenbsmith@gmail.com>
2015-08-12 07:40:54 -0700

    Wed morning

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,8 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user_name = @post.user.name
+    @cities = City.all.by_name
+    
 
     render :show
   end
