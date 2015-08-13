e47b71ef71402c0b995a3c5f2447f0dfae60d1c7
Amanda <amandaannegeorge@gmail.com>
2015-08-12 17:40:39 -0700

    migration to add city id to users, added city dropdown to signup form, updated city and user models, updated views with city info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  	def current_user
-  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  	end
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
 
-  	helper_method :current_user
+	helper_method :current_user
 
-  	def authorize
-  		unless current_user
-        # flash[:error] = "You must be logged in to do that."
-  			redirect_to login_path
-  		end
-  	end
+	def authorize
+		unless current_user
+      # flash[:error] = "You must be logged in to do that."
+			redirect_to login_path
+		end
+	end
 end
